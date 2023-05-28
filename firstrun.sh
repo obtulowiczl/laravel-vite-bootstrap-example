6e824e4d42de2853bdc7e6539d74099fb927f24f
nexiu <obtulowiczl@gmail.com>
2023-05-28 14:38:17 +0200

    Autoremoving script after first run.

--- firstrun.sh
+++ firstrun.sh
@@ -5,3 +5,4 @@ docker-compose exec --user=sail apache composer update && \
 docker-compose exec --user=sail apache npm i && \
 docker-compose exec --user=sail apache npm run build
 echo "Everything should be okay, add 0.0.0.0 local.test to your /eth/hosts and try visit it!"
+rm ./firstrun.sh
